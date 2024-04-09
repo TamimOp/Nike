eaa18d7b302e22820f6c2e6ac0009304ceb4a409
TamimOp <tamimshadanik@gmail.com>
2024-04-10 05:45:29 +0600

    10.4.24(1)

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopulerProducts from "./PopulerProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+    Hero,
+    PopulerProducts,
+    SpecialOffer,
+    Services,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Footer
+}
